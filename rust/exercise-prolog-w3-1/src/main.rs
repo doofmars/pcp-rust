ce1a5270b73e8ef985e6100aa92ff8f75dfa8d1d
Preuss Jan-Henrik <jan-henrik.preuss@stud.hslu.ch>
2024-05-03 13:07:35 +0200

    Shorten function call to prevent wrapping

--- rust/exercise-prolog-w3-1/src/main.rs
+++ rust/exercise-prolog-w3-1/src/main.rs
@@ -15,26 +15,26 @@ fn main() {
         .expect("Failed to read line");
     // Trim and parse to int 32
     let n: i32 = input_line.trim().parse().expect("Input not an integer");
-    let fib = fibonacci_recursive(n);
+    let fib = fibonacci_rec(n);
     print!("Die {}. Fibonacci-Zahl ist: {}", n, fib);
 }
 
-fn fibonacci_recursive(n: i32) -> u64 {
+fn fibonacci_rec(n: i32) -> u64 {
     if n < 0 {
         panic!("{} is negative!", n);
     }
     match n {
         0 => 0,
         1 | 2 => 1,
-        _ => fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2),
+        _ => fibonacci_rec(n - 1) + fibonacci_rec(n - 2),
     }
 }
 
 #[test]
-fn test_fibonacci_recursive() {
-    assert_eq!(0, fibonacci_recursive(0));
-    assert_eq!(1, fibonacci_recursive(1));
-    assert_eq!(1, fibonacci_recursive(2));
-    assert_eq!(2, fibonacci_recursive(3));
-    assert_eq!(3, fibonacci_recursive(4));
+fn test_fibonacci_rec() {
+    assert_eq!(0, fibonacci_rec(0));
+    assert_eq!(1, fibonacci_rec(1));
+    assert_eq!(1, fibonacci_rec(2));
+    assert_eq!(2, fibonacci_rec(3));
+    assert_eq!(3, fibonacci_rec(4));
 }
