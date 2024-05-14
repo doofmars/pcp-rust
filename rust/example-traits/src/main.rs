68ecb04dd18a5df75879f9cc941cf2667c951441
Roman Schilter <roman.schilter@stud.hslu.ch>
2024-05-14 17:01:52 +0200

    Improve trait example

--- rust/example-traits/src/main.rs
+++ rust/example-traits/src/main.rs
@@ -1,18 +1,28 @@
+// trait to with default implementation of the get_val function
 trait Foo {
     fn get_val() -> String {
         String::from("foo")
     }
 }
 
-struct Bar {}
+// structs without any fields
+struct Bar;
+struct Baz;
+
+// implement the trait "Foo" for the struct "Bar"
 impl Foo for Bar {
+    // override the get_val function
     fn get_val() -> String {
         String::from("bar")
     }
 }
 
+// because get_val has a default implementation, it does not need to be overriden
+impl Foo for Baz {}
+
 fn main() {
-    println!("Output: {}", Bar::get_val())
+    println!("get_val overridden: {}", Bar::get_val());
+    println!("get_val not overridden: {}", Baz::get_val());
 }
 
 #[test]
@@ -20,5 +30,10 @@ fn test() {
     assert_eq!(
         Bar::get_val(),
         "bar"
-    )
+    );
+
+    assert_eq!(
+        Baz::get_val(),
+        "foo"
+    );
 }
