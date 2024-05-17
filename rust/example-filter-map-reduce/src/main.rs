84290d72ea3f2270ac871c9db40f52200465e589
Preuss Jan-Henrik <jan-henrik.preuss@stud.hslu.ch>
2024-05-17 22:17:27 +0200

    Alter implementation to allow arrays with dynamic length

--- rust/example-filter-map-reduce/src/main.rs
+++ rust/example-filter-map-reduce/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let result = filter_map_reduce(
-        ["Tim", "Kathy", "Tamara", "Bob"]
+        &["Tim", "Kathy", "Tamara", "Bob"]
     );
 
     println!("Result: {}", result);
 }
 
-fn filter_map_reduce(list: [&str; 4]) -> String {
+// Here we could use the type [&str; 4] for list instead to fix to exactly 4 values in the list
+fn filter_map_reduce(list: &[&str]) -> String {
     list.iter()
         .filter(|e| e.starts_with("T"))
         .map(|e| e.to_uppercase())
@@ -16,7 +17,7 @@ fn filter_map_reduce(list: [&str; 4]) -> String {
 #[test]
 fn test_filter_map_reduce() {
     assert_eq!(
-        filter_map_reduce(["Tim", "Dong", "Tom", "Dang"]),
+        filter_map_reduce(&["Tim", "Dong", "Tom", "Dang"]),
         "TIM TOM"
     )
 }
