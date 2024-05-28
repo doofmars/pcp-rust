761b9f65843bd04089bdfbf7dbf18dc47433d2b1
Preuss Jan-Henrik <jan-henrik.preuss@stud.hslu.ch>
2024-05-28 17:01:01 +0200

    Rework code example based on feedback

--- rust/example-patterns/src/main.rs
+++ rust/example-patterns/src/main.rs
@@ -1,5 +1,8 @@
-// First pattern example using a matcher and option value
-fn matcher1_option_value(candidate: Option<i32>) -> Option<i32> {
+// Patterns covered here are based on the places for patterns of the rust book
+// https://doc.rust-lang.org/book/ch18-01-all-the-places-for-patterns.html
+
+// First pattern example using a pattern with match and option value
+fn pattern1_option_value(candidate: Option<i32>) -> Option<i32> {
     match candidate {
         None => None,
         // match if we have some value, then return the increment
@@ -7,27 +10,36 @@ fn matcher1_option_value(candidate: Option<i32>) -> Option<i32> {
     }
 }
 
-fn matcher2_if_let(candidate: &str) -> i8 {
+// Pattern to evaluate parsing result
+fn pattern2_if_let(candidate: &str) -> i8 {
+    // Parse the string to type i8 wrapped as a Result.
+    // The type used in the result determines the type to parse to.
     let result_value: Result<i8, _> = candidate.parse();
-    // Return parsing result matching the pattern "let Ok(age) = age"
     // The inner let reassigns result_value to the parsed variable of type i8
     // otherwise 0 if parsing is not Ok
     return if let Ok(result_value) = result_value {
+        // Variable can be returned as type is i8
         result_value
     } else {
+        // Error case
         0
-    }
+    };
 }
 
-fn matcher3_while_let(mut stack: Vec<i32>) {
+// Usage of pattern in while loop
+fn pattern3_while_let(mut stack: Vec<i32>) {
     // Pop the last element of the stack until it is empty
+    // Some is required here as the return type of Pop is Option
+    // Some will unwrap the value to the variable top if present
+    // Once the end is reached the loop will break due to a type mismatch
     while let Some(top) = stack.pop() {
-        println!("{}", top);
+        // Top is now the resolved i32 value from the Vector
+        println!("{:?}", top);
     }
 }
 
-fn matcher4_multiple_ranges(candidate: i32) -> i32 {
-    // Matcher with pattern for "or" and "range" (3-10)
+// Matcher with pattern for "or" and "range" (3-10)
+fn pattern5_multiple_ranges(candidate: i32) -> i32 {
     match candidate {
         1 | 2 => {
             println!("one or two");
@@ -42,22 +54,23 @@ fn matcher4_multiple_ranges(candidate: i32) -> i32 {
 }
 
 fn main() {
-    matcher1_option_value(None);
-    matcher2_if_let("23");
-    matcher3_while_let(vec![1, 2, 3]);
+    pattern1_option_value(None);
+    pattern2_if_let("23");
+    pattern3_while_let(vec![1, 2, 3]);
+    pattern5_multiple_ranges(4);
 }
 
 #[test]
 fn test_filter_map_reduce() {
-    assert_eq!(matcher1_option_value(None), None);
-    assert_eq!(matcher1_option_value(Option::from(4)), Some(5));
-    assert_eq!(matcher2_if_let("wrong"), 0);
-    assert_eq!(matcher2_if_let("10"), 10);
-    assert_eq!(matcher4_multiple_ranges(0), 0);
-    assert_eq!(matcher4_multiple_ranges(1), 1);
-    assert_eq!(matcher4_multiple_ranges(2), 1);
-    assert_eq!(matcher4_multiple_ranges(3), 2);
-    assert_eq!(matcher4_multiple_ranges(5), 2);
-    assert_eq!(matcher4_multiple_ranges(10), 2);
-    assert_eq!(matcher4_multiple_ranges(11), 0);
+    assert_eq!(pattern1_option_value(None), None);
+    assert_eq!(pattern1_option_value(Option::from(4)), Some(5));
+    assert_eq!(pattern2_if_let("wrong"), 0);
+    assert_eq!(pattern2_if_let("10"), 10);
+    assert_eq!(pattern5_multiple_ranges(0), 0);
+    assert_eq!(pattern5_multiple_ranges(1), 1);
+    assert_eq!(pattern5_multiple_ranges(2), 1);
+    assert_eq!(pattern5_multiple_ranges(3), 2);
+    assert_eq!(pattern5_multiple_ranges(5), 2);
+    assert_eq!(pattern5_multiple_ranges(10), 2);
+    assert_eq!(pattern5_multiple_ranges(11), 0);
 }
